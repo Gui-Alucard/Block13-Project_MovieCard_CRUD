8e5ce0887832d1ac3889c17206a55e1838342a18
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-09-26 10:41:04 -0300

    Updates

---
+++ precache-manifest.acc65ee9922240167de7c2a843a2741b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6462e77195454ed1f58b296b904d03dd",
+    "url": "/Block13-Project_MovieCard_CRUD/index.html"
+  },
+  {
+    "revision": "4db396efea9480b49e9d",
+    "url": "/Block13-Project_MovieCard_CRUD/static/css/main.797648b3.chunk.css"
+  },
+  {
+    "revision": "3bec86b85242d97b53ff",
+    "url": "/Block13-Project_MovieCard_CRUD/static/js/2.40e48109.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Block13-Project_MovieCard_CRUD/static/js/2.40e48109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db396efea9480b49e9d",
+    "url": "/Block13-Project_MovieCard_CRUD/static/js/main.d91ec53c.chunk.js"
+  },
+  {
+    "revision": "a7f053307b6df932d541",
+    "url": "/Block13-Project_MovieCard_CRUD/static/js/runtime-main.28784db1.js"
+  },
+  {
+    "revision": "5e512cfb57ee00aa6865badb620e737e",
+    "url": "/Block13-Project_MovieCard_CRUD/static/media/movie-background.5e512cfb.jpg"
+  },
+  {
+    "revision": "7790a3934f8a9dec03a30ed666ea1ced",
+    "url": "/Block13-Project_MovieCard_CRUD/static/media/movie-icon.7790a393.png"
+  }
+]);
